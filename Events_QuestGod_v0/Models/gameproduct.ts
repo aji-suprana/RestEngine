2a67c87c1508c26c3ba9bf0da0df8cd10e7e1850
asuprana <ajiadiwenas@gmail.com>
2019-04-26 11:45:51 +0700

    gameproducts.ts: modified required request properties

--- Events_QuestGod_v0/Models/gameproduct.ts
+++ Events_QuestGod_v0/Models/gameproduct.ts
@@ -4,13 +4,14 @@ export interface IGameProduct extends Document{
   _id:mongoose.Schema.Types.ObjectId,
   issuer_id:mongoose.Schema.Types.ObjectId,
   name:string,
-
+  productType: string
 }
 
 let gameproductSchema = new Schema({
   _id:mongoose.Schema.Types.ObjectId,
-  issuer_id:mongoose.Schema.Types.ObjectId,
-  name: {type:String,required:true}
+  issuer_id:{type:mongoose.Schema.Types.ObjectId,required:true},
+  name: {type:String,required:true},
+  productType:{type:String,required:true}
 });
 
 export default mongoose.model<IGameProduct>('GameProduct', gameproductSchema);
