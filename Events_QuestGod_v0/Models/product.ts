fe9240c022f17360f157a9002a0cc39f5fc95e51
edosetiawan <edo.setiawan@voxelsoftware.co>
2019-05-11 15:07:48 +0700

    Create product post, delete, get, update method & quest post, get method

--- Events_QuestGod_v0/Models/product.ts
+++ Events_QuestGod_v0/Models/product.ts
@@ -4,14 +4,17 @@ export interface IProduct extends Document{
   _id:mongoose.Schema.Types.ObjectId,
   owner:mongoose.Types.ObjectId
   name:string,
-  productType: string
+  productType: string,
+  quest: mongoose.Schema.Types.ObjectId,
 }
 
 let productSchema = new Schema({
   _id:mongoose.Schema.Types.ObjectId,
   owner: {type:mongoose.Types.ObjectId,required:true,index:true},
   name: {type:String,required:true},
-  productType:{type:String,required:true}
+  productType:{type:String,required:true},
+  quest: {type:[mongoose.Schema.Types.ObjectId],required :true}
+
 });
 
 export default mongoose.model<IProduct>('Product', productSchema);
--- Events_QuestGod_v0/RG_Products.ts
+++ Events_QuestGod_v0/RG_Products.ts
@@ -6,8 +6,10 @@ import {RequestGroup} from'../Engine/BaseClass/RequestsGroup'
 
 import {checkAuth} from '../Events_Authentication_v0/Middleware/check-auth'
 //Requests
+import {ProductGet} from "./RequestHandler/ProductGet"
 import {ProductCreate} from "./RequestHandler/ProductCreate"
 import {ProductDelete} from "./RequestHandler/ProductDelete"
+import {ProductUpdate} from "./RequestHandler/ProductUpdate"
 
 import {QuestCreate} from "./RequestHandler/QuestCreate"
 import {QuestDelete} from "./RequestHandler/QuestDelete"
@@ -36,13 +38,15 @@ export class RG_Products extends RequestGroup
     {
         console.log("Registering Child Methods in " + this.requestGroupPath)
         //products
+        this.RegisterRGChildMethod(HTTPMethodType.get,"", checkAuth, ProductGet);
         this.RegisterRGChildMethod(HTTPMethodType.post,"",checkAuth,ProductCreate);
-        this.RegisterRGChildMethod(HTTPMethodType.delete,':productId',checkAuth,ProductDelete);
+        this.RegisterRGChildMethod(HTTPMethodType.delete,'',checkAuth,ProductDelete);
+        this.RegisterRGChildMethod(HTTPMethodType.patch, '', checkAuth, ProductUpdate);
 
         //quests
         this.RegisterRGChildMethod(HTTPMethodType.post,"quest/:productId",checkAuth,QuestCreate);
         this.RegisterRGChildMethod(HTTPMethodType.post,"quest/enroll/:questId",checkAuth,QuestEnroll);
-        this.RegisterRGChildMethod(HTTPMethodType.post,"quest",QuestGet);
+        this.RegisterRGChildMethod(HTTPMethodType.get,"quest",QuestGet);
         this.RegisterRGChildMethod(HTTPMethodType.patch,"quest/:questId",checkAuth,QuestUpdate);
         this.RegisterRGChildMethod(HTTPMethodType.delete,"quest/:questId",checkAuth,QuestDelete);
         
