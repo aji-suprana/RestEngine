5e403397f87310be616f37f827392ebf0f053ec1
edosetiawan <edo.setiawan@voxelsoftware.co>
2019-06-13 19:41:59 +0700

    add point

--- Events_Authentication_v0/Models/user.ts
+++ Events_Authentication_v0/Models/user.ts
@@ -4,7 +4,8 @@ export interface IUser extends Document{
   _id:mongoose.Schema.Types.ObjectId,
   email:string,
   password:string,
-  userType: string
+  userType: string,
+  point: Number
 }
 
 let userSchema = new Schema({
@@ -15,7 +16,9 @@ let userSchema = new Schema({
         unique:true,
         match:/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
   },
-  password:{ type: String, required:true}
+  password:{ type: String, required:true},
+  userType: { type: String, required: true},
+  point: {type: Number, required: true}
 });
 
 export default mongoose.model<IUser>('User', userSchema);
